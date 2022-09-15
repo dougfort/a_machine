3c6b66d3438c78b66d231f076d243dfebc57fcc6
dougfort <doug.fort@gmail.com>
2022-09-15 15:39:45 -0400

    start over without tiles

--- src/main.rs
+++ src/main.rs
@@ -1,103 +1,38 @@
-use bevy::{prelude::*, render::texture::ImageSettings};
-use bevy_ecs_tilemap::prelude::*;
+use bevy::prelude::*;
+use rand::Rng;
 
-mod helpers;
-
-fn startup(mut commands: Commands, asset_server: Res<AssetServer>) {
-    commands.spawn_bundle(Camera2dBundle::default());
-
-    let texture_handle: Handle<Image> = asset_server.load("tiles.png");
-
-    let tilemap_size = TilemapSize { x: 32, y: 32 };
-
-    // Create a tilemap entity a little early.
-    // We want this entity early because we need to tell each tile which tilemap entity
-    // it is associated with. This is done with the TilemapId component on each tile.
-    // Eventually, we will insert the `TilemapBundle` bundle on the entity, which
-    // will contain various necessary components, such as `TileStorage`.
-    let tilemap_entity = commands.spawn().id();
-
-    // To begin creating the map we will need a `TileStorage` component.
-    // This component is a grid of tile entities and is used to help keep track of individual
-    // tiles in the world. If you have multiple layers of tiles you would have a tilemap entity
-    // per layer, each with their own `TileStorage` component.
-    let mut tile_storage = TileStorage::empty(tilemap_size);
-
-    // Spawn the elements of the tilemap.
-    // Alternatively, you can use helpers::fill_tilemap.
-    for x in 0..tilemap_size.x {
-        for y in 0..tilemap_size.y {
-            let tile_pos = TilePos { x, y };
-            let tile_entity = commands
-                .spawn()
-                .insert_bundle(TileBundle {
-                    position: tile_pos,
-                    tilemap_id: TilemapId(tilemap_entity),
-                    ..Default::default()
-                })
-                .id();
-            tile_storage.set(&tile_pos, Some(tile_entity));
-        }
-    }
-
-    let tile_size = TilemapTileSize { x: 16.0, y: 16.0 };
-    let grid_size = TilemapGridSize { x: 16.0, y: 16.0 };
-
-    commands
-        .entity(tilemap_entity)
-        .insert_bundle(TilemapBundle {
-            grid_size,
-            size: tilemap_size,
-            storage: tile_storage,
-            texture: TilemapTexture(texture_handle),
-            tile_size,
-            transform: get_centered_transform_2d(&tilemap_size, &tile_size, 0.0),
-            ..Default::default()
-        });
-}
-
-fn swap_texture_or_hide(
-    asset_server: Res<AssetServer>,
-    keyboard_input: Res<Input<KeyCode>>,
-    mut query: Query<(&mut TilemapTexture, &mut Visibility)>,
-) {
-    if keyboard_input.just_pressed(KeyCode::Space) {
-        let texture_handle_a: Handle<Image> = asset_server.load("tiles.png");
-        let texture_handle_b: Handle<Image> = asset_server.load("tiles2.png");
-        for (mut tilemap_tex, _) in &mut query {
-            if tilemap_tex.0 == texture_handle_a {
-                tilemap_tex.0 = texture_handle_b.clone();
-            } else {
-                tilemap_tex.0 = texture_handle_a.clone();
-            }
-        }
-    }
-    if keyboard_input.just_pressed(KeyCode::H) {
-        for (_, mut visibility) in &mut query {
-            if visibility.is_visible {
-                visibility.is_visible = false;
-            } else {
-                visibility.is_visible = true;
-            }
-        }
-    }
-}
 
 fn main() {
     App::new()
-        .insert_resource(WindowDescriptor {
-            width: 1270.0,
-            height: 720.0,
-            title: String::from(
-                "Basic Example - Press Space to change Texture and H to show/hide tilemap.",
-            ),
-            ..Default::default()
-        })
-        .insert_resource(ImageSettings::default_nearest())
         .add_plugins(DefaultPlugins)
-        .add_plugin(TilemapPlugin)
-        .add_startup_system(startup)
-        .add_system(helpers::camera::movement)
-        .add_system(swap_texture_or_hide)
+        .add_startup_system(setup)
         .run();
+}
+
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    const SPRITE_WIDTH: f32 = 32.0;
+    const SPRITE_COUNT: usize = 10;
+    let blank_sprite: Handle<Image> = asset_server.load("blank.png");
+    let zero_sprite: Handle<Image> = asset_server.load("zero.png");
+    let one_sprite: Handle<Image> = asset_server.load("one.png");
+    let mut rng = rand::thread_rng();
+
+    commands.spawn_bundle(Camera2dBundle::default());
+
+    for i in 0..SPRITE_COUNT {
+        let sprite = match rng.gen_range(0..3) {
+            0 => zero_sprite.clone(),
+            1 => one_sprite.clone(),
+            _ => blank_sprite.clone(),
+        };
+        commands.spawn_bundle(SpriteBundle {
+            texture: sprite,
+            transform: Transform::from_translation(Vec3::new(
+                (i as f32 - (SPRITE_COUNT as f32 / 2.0)) * SPRITE_WIDTH,
+                0.0,
+                0.0,
+            )),
+            ..Default::default()
+        });
+    }
 }
