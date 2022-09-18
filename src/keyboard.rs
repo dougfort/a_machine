fd52b49d2aacd9e7ebfd0c213967740f42bb35fd
dougfort <doug.fort@gmail.com>
2022-09-18 16:57:45 -0400

    Use keys to modify the tape

--- src/keyboard.rs
+++ src/keyboard.rs
@@ -9,9 +9,26 @@ pub fn keyboard_input(
     args: Res<cli::Args>,
     commands: Commands,
     sprites: Res<sprites::Sprites>,
-    tape: Res<tape::Tape>
+    mut tape: ResMut<tape::Tape>
 ) {
-    if args.step && keys.just_pressed(KeyCode::Space) {
+    if keys.just_pressed(KeyCode::Space) {
         println!("Space");
+        tape.set(tape::Alphabet::Blank);
+    }
+    if keys.just_pressed(KeyCode::Key0) {
+        println!("0");
+        tape.set(tape::Alphabet::Zero);
+    }
+    if keys.just_pressed(KeyCode::Key1) {
+        println!("1");
+        tape.set(tape::Alphabet::One);
+    }
+    if keys.just_pressed(KeyCode::Left) {
+        println!("Left");
+        tape.move_left();
+    }
+    if keys.just_pressed(KeyCode::Right) {
+        println!("Right");
+        tape.move_right();
     }
 }
